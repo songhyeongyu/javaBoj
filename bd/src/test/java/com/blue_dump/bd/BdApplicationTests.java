13c1868dff3735370366902a23d27430da6d22f3
song hyeongyu <lit3207@ajou.ac.kr>
2024-10-16 10:55:04 +0900

    inital commit

---
+++ bd/src/test/java/com/blue_dump/bd/BdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blue_dump.bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
