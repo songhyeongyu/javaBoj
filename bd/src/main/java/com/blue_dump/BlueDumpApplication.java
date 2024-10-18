f5021f821fe60f34e48b056b2ae46deb74d5dc20
song hyeongyu <lit3207@ajou.ac.kr>
2024-10-18 17:29:57 +0900

    revise application

---
+++ bd/src/main/java/com/blue_dump/BlueDumpApplication.java
@@ -0,0 +1,10 @@
+package com.blue_dump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class BlueDumpApplication {
+    public static void main(String[] args) {SpringApplication.run(BlueDumpApplication.class, args);}
+}
