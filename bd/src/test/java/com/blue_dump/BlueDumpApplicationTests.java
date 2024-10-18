d4b6555438f614f2d7dcb828acd0b9754e02493b
song hyeongyu <lit3207@ajou.ac.kr>
2024-10-18 17:34:34 +0900

    test revise test

--- bd/src/test/java/com/blue_dump/BlueDumpApplicationTests.java
+++ bd/src/test/java/com/blue_dump/BlueDumpApplicationTests.java
@@ -1,13 +1,13 @@
-package com.blue_dump.bd;
+package com.blue_dump;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BdApplicationTests {
+class BlueDumpApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
