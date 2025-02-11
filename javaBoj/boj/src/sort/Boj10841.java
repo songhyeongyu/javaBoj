f23504c151ab01a903bf154d321759d5cc9bd3c3
song hyeongyu <lit3207@ajou.ac.kr>
2025-02-11 15:05:28 +0900

    feat : add Boj10841 solution

---
+++ javaBoj/boj/src/sort/Boj10841.java
@@ -0,0 +1,70 @@
+package sort;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Boj10841{
+    static ArrayList<User> users = new ArrayList<>();
+    static int N;
+
+
+    public static void main(String[] args) throws IOException {
+        Boj10841 process = new Boj10841();
+        process.run();
+
+    }
+
+    public void run() throws IOException {
+        init();
+        sortUsers();
+        printUser();
+    }
+
+    public void init() throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            users.add(new User(age, name, i));
+        }
+        sortUsers();
+
+    }
+
+    public void sortUsers() {
+        Collections.sort(users, new Comparator<User>(){
+            @Override
+            public int compare(User o1, User o2) {
+                if (o1.age == o2.age) {
+                    return Integer.compare(o1.idx, o2.idx);
+                }
+                return Integer.compare(o1.age, o2.age);
+            }
+        });
+    }
+
+    public void printUser() {
+        for (User user : users) {
+            System.out.println(user.age + " " + user.name);
+        }
+    }
+
+
+}
+
+class User {
+    int age;
+    String name;
+    int idx;
+
+    public User(int age, String name, int idx) {
+        this.age = age;
+        this.name = name;
+        this.idx = idx;
+    }
+}
