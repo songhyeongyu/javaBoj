142aaee3e5d86435fb80f7b5dd49d2acebfc0b80
song hyeongyu <lit3207@ajou.ac.kr>
2025-02-20 14:20:31 +0900

    fix : try to solve Boj2146

--- javaBoj/boj/src/bfs/Boj2146.java
+++ javaBoj/boj/src/bfs/Boj2146.java
@@ -6,76 +6,137 @@ import java.io.*;
 
 public class Boj2146 {
     static int N;
-    static int[][] island;
-    static boolean[][] visited;
-    static int[][] dist;
 
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
+    static int[][] board;
+    static int[][] dist;
+    static Deque<Node> dq = new LinkedList<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         Boj2146 process = new Boj2146();
         process.run();
     }
 
-    private void run() throws IOException {
+    private void run() throws IOException{
         init();
-        findLand();
-        System.out.println(Arrays.deepToString(island));
-    }
+        findIslandNum();
+        findAnotherLand();
 
+        for (int i = 0; i < N; i++) {
+            System.out.println(Arrays.toString(dist[i]));
+        }
+
+    }
+    
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(bf.readLine());
-        island = new int[N][N];
-        visited = new boolean[N][N];
+
+        board = new int[N][N];
         dist = new int[N][N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             for (int j = 0; j < N; j++) {
-                int input = Integer.parseInt(st.nextToken());
-                island[i][j] = input;
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dist[i], -1);
+        }
+        
     }
 
-    private void findLand() {
+    private void findIslandNum() {
         int idx = 1;
+        boolean[][] isVisit = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (visited[i][j] || island[i][j] == 0) {
-                    continue;
-                }
-                Queue<Node> q = new LinkedList<>();
-                q.offer(new Node(i, j));
-                visited[i][j] = true;
-                while (!q.isEmpty()) {
-                    Node cur = q.poll();
-                    island[cur.x][cur.y] = idx;
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = cur.x + dx[dir];
-                        int ny = cur.y + dy[dir];
-
-                        if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny] || island[nx][ny] == 0) {
-                            continue;
-                        }
-                        q.offer(new Node(nx, ny));
-                        visited[nx][ny] = true;
-                        island[nx][ny] = idx;
-                    }
+                if(board[i][j] != 0 && !isVisit[i][j]){
+                    bfs(i, j, idx,isVisit);
+                    idx++;
                 }
             }
         }
 
     }
+
+    private void bfs(int x, int y,int idx, boolean[][] isVisit) {
+        Deque<Node> island = new LinkedList<>();
+
+
+        board[x][y] = idx;
+        dist[x][y] = 0;
+        island.offer(new Node(x, y, idx));
+        isVisit[x][y] = true;
+
+        while (!island.isEmpty()) {
+            Node cur = island.pollFirst();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                    continue;
+                }
+
+                if (board[nx][ny] == 0 || isVisit[nx][ny]) {
+                    continue;
+                }
+
+                board[nx][ny] = idx;
+                isVisit[nx][ny] = true;
+                island.offer(new Node(nx, ny, idx));
+                dq.offer(new Node(nx, ny, idx));
+                dist[nx][ny] = 0;
+            }
+        }
+    }
+
+    private void findAnotherLand() {
+
+        int ans = Integer.MAX_VALUE;
+        while (!dq.isEmpty()) {
+            Node cur = dq.pollFirst();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                    continue;
+                }
+
+                if (board[nx][ny] != 0 && cur.islandNumber != board[nx][ny]) {
+                    ans = Math.min(ans, dist[cur.x][cur.y]);
+                    continue;
+                }
+
+                if(dist[nx][ny] == -1 && board[nx][ny] == 0) {
+                    dist[nx][ny] = dist[cur.x][cur.y] + 1;
+                    dq.offer(new Node(nx, ny, cur.islandNumber));
+                }
+            }
+
+        }
+        System.out.println(ans);
+    }
+
 
     static class Node {
         int x;
         int y;
+        int islandNumber;
 
-        public Node(int x, int y) {
+        public Node(int x, int y, int islandNumber) {
             this.x = x;
             this.y = y;
+            this.islandNumber = islandNumber;
         }
     }
+
 }
