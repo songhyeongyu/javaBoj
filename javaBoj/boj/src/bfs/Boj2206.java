a8c4e6a22e495690909571ab35dcd2535863d788
song hyeongyu <lit3207@ajou.ac.kr>
2025-02-20 09:27:04 +0900

    feat : add Boj2206 solution

--- javaBoj/boj/src/bfs/Boj2206.java
+++ javaBoj/boj/src/bfs/Boj2206.java
@@ -13,7 +13,7 @@ public class Boj2206 {
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
 
-    static Deque<Node> deque = new LinkedList<>();
+    static Deque<Node> dq = new LinkedList<>();
 
 
     public static void main(String[] args) throws IOException{
@@ -35,11 +35,11 @@ public class Boj2206 {
         M = Integer.parseInt(st.nextToken());
         board = new char[N][M];
         dist = new int[N][M][2];
+
         for (int i = 0; i < N; i++) {
             String input = bf.readLine();
             for (int j = 0; j < M; j++) {
-                char c = input.charAt(j);
-                board[i][j] = c;
+                board[i][j] = input.charAt(j);
                 dist[i][j][0] = -1;
                 dist[i][j][1] = -1;
             }
@@ -47,17 +47,17 @@ public class Boj2206 {
     }
 
     private int bfs() {
-        deque.offer(new Node(0, 0, 0));
+        dq.offer(new Node(0, 0, 0));
         dist[0][0][0] = dist[0][0][1] = 0;
 
+        while (!dq.isEmpty()) {
+            Node cur = dq.pollFirst();
+            int nxt = dist[cur.x][cur.y][cur.broken] + 1;
 
-        while (!deque.isEmpty()) {
-            Node cur = deque.pollFirst();
             if (cur.x == N - 1 && cur.y == M - 1) {
-                return dist[cur.x][cur.y][cur.broken];
+                return dist[N - 1][M - 1][cur.broken];
             }
 
-            int nxt = dist[cur.x][cur.y][cur.broken] + 1;
             for (int i = 0; i < 4; i++) {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
@@ -65,20 +65,20 @@ public class Boj2206 {
                 if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
                     continue;
                 }
-                //벽이 안부서진 경우
-                if (board[nx][ny] == '0' && dist[nx][ny][cur.broken] == -1) {
+
+                if (dist[nx][ny][cur.broken] == -1 && board[nx][ny] == '0') {
                     dist[nx][ny][cur.broken] = nxt;
-                    deque.offer(new Node(nx, ny, cur.broken));
+                    dq.offer(new Node(nx, ny, cur.broken));
                 }
 
-                //벽을 부순 경우
-                if (cur.broken != 1 && board[nx][ny] == '1' && dist[nx][ny][cur.broken] == -1) {
+                if (dist[nx][ny][cur.broken] == -1 && board[nx][ny] == '1' && cur.broken == 0) {
                     dist[nx][ny][1] = nxt;
-                    deque.offer(new Node(nx, ny, 1));
+                    dq.offer(new Node(nx, ny, 1));
                 }
             }
         }
         return -1;
+
     }
 
     static class Node {
