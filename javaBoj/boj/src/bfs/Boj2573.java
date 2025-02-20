b7823d50332ba3584af72d5e561aeea307c5cc5e
song hyeongyu <lit3207@ajou.ac.kr>
2025-02-20 11:37:58 +0900

    fix : try to solve Boj2573

--- javaBoj/boj/src/bfs/Boj2573.java
+++ javaBoj/boj/src/bfs/Boj2573.java
@@ -6,16 +6,15 @@ import java.io.*;
 public class Boj2573 {
     static int N;
     static int M;
+    static int after;
 
     static int[][] ice;
-    static int[][] visited;
-    static boolean[][] isIce;
+    static boolean[][][] isIce;
 
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
 
-    static Deque<Node> deque = new LinkedList<>();
-    static Deque<Node> meltingICe = new LinkedList<>();
+    static Deque<Node> dq = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         Boj2573 process = new Boj2573();
@@ -24,99 +23,102 @@ public class Boj2573 {
 
     private void run() throws IOException {
         init();
-        System.out.println(checkIce());
-//        System.out.println(Arrays.deepToString(visited));
+        System.out.println(searchMelting());
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         ice = new int[N][M];
-        isIce = new boolean[N][M];
-        visited = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(visited[i], -1);
-        }
+        isIce = new boolean[N][M][1000001];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(bf.readLine());
             for (int j = 0; j < M; j++) {
-                int year = Integer.parseInt(st.nextToken());
-                if (year > 0) {
-                    deque.offer(new Node(i, j));
-                    isIce[i][j] = true;
-                    visited[i][j] = 0;
+                int num = Integer.parseInt(st.nextToken());
+                if (num > 0) {
+                    dq.offer(new Node(i, j));
                 }
-                ice[i][j] = year;
+                ice[i][j] = num;
             }
         }
-
     }
 
-    private int checkIce() {
-        int year = 0;
-        while (true) {
-            if (deque.isEmpty()) {
-                return 0; // 빙산이 전부 녹았으면 즉시 종료
+    private int searchMelting() {
+        int now = 0;
+        int cnt = 0;
+
+        while (!dq.isEmpty()) {
+            boolean isZero = false;
+            for (Node node : dq) {
+
             }
-            Deque<Node> temp = new LinkedList<>(deque);
-            int iter = 0;
-            while (!temp.isEmpty()) {
-                Node cur = temp.pollFirst();
-                if (ice[cur.x][cur.y] == 0) {
-                    isIce[cur.x][cur.y] = false;
-                    continue;
-                }
-                ice[cur.x][cur.y] -= 1;
-                iter++;
-                if (iter == temp.size()) {
-                    break;
+            now++;
+
+            Iterator<Node> iterator = dq.iterator();
+            while (iterator.hasNext()) {
+                Node node = iterator.next();
+                if (ice[node.x][node.y] == 0) {
+                    isZero = true;
+                    isIce[node.x][node.y][now] = true;
+                    iterator.remove();
                 }
             }
 
-            int count = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (isIce[i][j] && ice[i][j] > 0 && visited[i][j] == 0) {
-                        bfs(i, j);
-                        count++;
+            if (isZero) {
+                int count = 0;
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < M; j++) {
+                        if (ice[i][j] != 0 && !isIce[i][j][now]) {
+                            bfs(i,j,now);
+                            count++;
+                        }
+                    }
+                    if (count > 1) {
+                        return now;
                     }
                 }
             }
 
-            if (count == 0) {
-                return 0; // 빙산이 다 녹았을 경우 종료
-            }
-            if (count > 1) {
-                break; // 빙산이 분리된 경우 종료
-            }
-            year++;
+
         }
-        return year;
+        return 0;
+
     }
 
-    private void bfs(int x, int y) {
-        meltingICe.offer(new Node(x, y));
-        visited[x][y] = 1; // 방문 체크 추가
 
-        while (!meltingICe.isEmpty()) {
-            Node cur = meltingICe.pollFirst();
+    private void bfs(int x,int y, int now) {
+        Deque<Node> deque = new LinkedList<>();
+        deque.add(new Node(x, y));
+        isIce[x][y][now] = true;
+
+        while (!deque.isEmpty()) {
+            Node cur = deque.pollFirst();
 
             for (int i = 0; i < 4; i++) {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                if (nx < 0 || nx >= N || ny < 0 || ny >=M) {
                     continue;
                 }
-                if (!isIce[nx][ny] || ice[nx][ny] == 0 || visited[nx][ny] == 1) {
+
+                if (ice[nx][ny] == 0) {
                     continue;
                 }
-                visited[nx][ny] = 1; // 방문 체크
-                meltingICe.offer(new Node(nx, ny));
+
+                if (isIce[nx][ny][now]) {
+                    continue;
+                }
+
+                isIce[nx][ny][now] = true;
+                deque.offer(new Node(nx, ny));
+
+
             }
         }
     }
