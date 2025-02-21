85cac2d1d08d5856b9c6f128848a9e8ac16398f8
song hyeongyu <lit3207@ajou.ac.kr>
2025-02-21 10:42:22 +0900

    fix : try to solve Boj13549

--- javaBoj/boj/src/bfs/Boj13549.java
+++ javaBoj/boj/src/bfs/Boj13549.java
@@ -10,6 +10,7 @@ public class Boj13549 {
     static int K;
     static int[] arr;
     static boolean[] visit;
+    static Deque<Integer> dq = new LinkedList<>();
 
     public static void main(String[] args) throws IOException{
         new Boj13549().run();
@@ -22,51 +23,48 @@ public class Boj13549 {
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
         swap();
+        arr = new int[K + 2];
+        visit = new boolean[K + 2];
+        bfs();
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(visit));
 
-        arr = new int[K+1];
-        visit = new boolean[K+1];
-
-        Arrays.fill(arr, -1);
-        arr[N] = 0;
-        visit[N] = true;
-        Deque<Integer> deque = new LinkedList<>();
-        deque.offer(N);
-
-        while (!deque.isEmpty()) {
-            int cur = deque.pollFirst();
-
-            int[] d = {2 * cur, cur + 1, cur - 1};
-
-            for (int i = 0; i < 3; i++) {
-                int nx = d[i];
-
-                if(nx < 0 || nx > K ){
-                    continue;
-                }
-
-                if (!visit[nx]) {
-                    if(i == 0) {
-                        arr[nx] = Math.min(arr[cur],arr[nx]);
-                        deque.offerFirst(nx);
-                    }else {
-                        arr[nx] = arr[cur] + 1;
-                        deque.offerLast(nx);
-                    }
-                    visit[nx] = true;
-                }
-            }
-            System.out.println(Arrays.toString(arr));
-        }
-
-        System.out.println(arr[K]);
 
     }
 
+    private void bfs() {
+        dq.offer(N);
+        visit[N] = true;
+        while (!dq.isEmpty()) {
+            Integer cur = dq.pollFirst();
+
+            if (cur > 0 && cur < K + 1) {
+                int now = 2 * cur;
+                if (!(now < 0 || now > K + 1 || visit[now])) {
+                    arr[2 * cur] = 0;
+                    dq.offerFirst(2 * cur);
+                    visit[2 * cur] = true;
+                }
+
+
+                int[] dir = {cur - 1, cur + 1};
+
+                for (int i = 0; i < 2; i++) {
+                    int now1 = dir[i];
+                    if (now1 < 0 || now1 >= K) {
+                        continue;
+                    }
+                    arr[now1] = arr[cur] + 1;
+                    visit[now1] = true;
+                    dq.offerLast(now1);
+                }
+            }
+        }
+
+    }
     private static void swap() {
         if (N > K) {
             int temp = N;
