01164d4712e7000d9009c9a57546bfcd022796eb
song hyeongyu <lit3207@ajou.ac.kr>
2025-02-26 16:34:39 +0900

    feat : add Boj17298 solution

--- javaBoj/boj/src/stack/Boj17298.java
+++ javaBoj/boj/src/stack/Boj17298.java
@@ -4,25 +4,56 @@ import java.util.*;
 import java.io.*;
 
 public class Boj17298 {
+
+    static int N;
+    static int[] arr;
+    static int[] ans;
+    static Stack<Integer> stack = new Stack<>();
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException{
+        Boj17298 process = new Boj17298();
+        process.run();
+    }
+
+    private void run() throws IOException {
+        init();
+        searchStack();
+        printResult();
+    }
+
+    private void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Stack<Integer> oge = new Stack<>();
-        StringBuilder sb = new StringBuilder();
+        arr = new int[N];
+        ans = new int[N];
+        Arrays.fill(ans, -1);
         for (int i = 0; i < N; i++) {
-            int element = Integer.parseInt(st.nextToken());
-            while (!oge.isEmpty() && (oge.peek() < element)) {
-                oge.pop();
-                sb.append(element).append(" ");
-            }
-            oge.push(element);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-//        sb.append("-1");
-        sb.insert(0, "-1");
-
-
     }
+
+    private void searchStack() {
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                ans[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+    }
+
+    private void printResult() {
+        for (int an : ans) {
+            sb.append(an).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+
+
+
+
 }
