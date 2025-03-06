b3d36853d29aa328fe19c6eae41e79162b50ce85
song hyeongyu <lit3207@ajou.ac.kr>
2025-03-06 13:04:16 +0900

    feat : try to solve Boj1600

--- javaBoj/boj/src/bfs/Boj1600.java
+++ javaBoj/boj/src/bfs/Boj1600.java
@@ -6,120 +6,130 @@ import java.util.*;
 public class Boj1600 {
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int[] horseDx = {2, 2, 1, -1, -2, -2, 1, -1};
-    static int[] horseDy = {1, -1, 2, 2, 1, -1, -2, -2};
 
-    static char[][] board;
+    static int[] dxH = {2, 1, -1, -2, -2, -1, 1, -2};
+    static int[] dyH = {1, 2, 2, 1, -1, -2, -2, -1};
+
+    static int k;
+    static int r;
+    static int c;
+
+    static int[][] board;
     static int[][][] dist;
-    static boolean[][][] visited;
-
-    static int K;
-    static int N;
-    static int M;
-
-    static Deque<Node> dq = new LinkedList<>();
-
+    static Queue<Node> queue = new LinkedList<>();
     public static void main(String[] args) throws IOException{
         Boj1600 process = new Boj1600();
         process.run();
-
     }
 
-    private void run() throws IOException{
+    private void run() throws IOException {
         init();
-        bfs();
-//        System.out.println(dist[N-1][M-1][0]);
-    }
+        bfs(0, 0);
 
+    }
+    /*2
+10 2
+0 0 1 0 0 1 0 0 1 0
+0 0 1 1 0 0 0 0 1 0*/
     private void init() throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        K = Integer.parseInt(bf.readLine());
+        k = Integer.parseInt(bf.readLine());
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        board = new char[N][M];
-        dist = new int[N][M][K+1];
-        visited = new boolean[N][M][K+1];
-        for (int i = 0; i < N; i++) {
+        c = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        board = new int[r][c];
+        dist = new int[32][r][c];
+        for (int i = 0; i < r; i++) {
             st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < M; j++) {
-                char c = st.nextToken().charAt(0);
-                board[i][j] = c;
+            for (int j = 0; j < c; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                dist[0][i][j] = -1;
             }
         }
-        for (int k = 0; k < K + 1; k++) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    dist[i][j][k] = -1;
-                }
+
+        for (int z = 0; z <= k; z++) {
+            for (int i = 0; i < r; i++) {
+                Arrays.fill(dist[z][i], -1);
             }
         }
 
     }
 
-    private void bfs() {
-        Deque<Node> dq = new LinkedList<>();
-        dq.offer(new Node(0, 0, K));
-        dist[0][0][K] = 0;
-        while (!dq.isEmpty()) {
-            Node cur = dq.pollFirst();
+    private void bfs(int x, int y) {
+        queue.offer(new Node(x, y,0));
+        dist[0][x][y] = 0;
 
-            if (cur.x == N - 1 && cur.y == M - 1) {
-                System.out.println(dist[cur.x][cur.y][cur.year]);
-                return;
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+
+
+            if (cur.cnt < k) {
+                BfsHorse(cur);
             }
 
-
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
-                    continue;
-                }
-
-                if (board[nx][ny] == 1||dist[nx][ny][cur.year] > 0) {
-                    continue;
-                }
-
-                dist[nx][ny][cur.year] = dist[cur.x][cur.y][cur.year] + 1;
-                dq.offer(new Node(nx, ny, cur.year));
-            }
-
-            if (cur.year > 0) {
-
-                for (int i = 0; i < 8; i++) {
-                    int nx = cur.x + horseDx[i];
-                    int ny = cur.y + horseDy[i];
-
-                    if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
-                        continue;
-                    }
-
-                    if (board[nx][ny] == 1 || dist[nx][ny][cur.year] > 0) {
-                        continue;
-                    }
-
-                    dist[nx][ny][cur.year - 1] = dist[nx][ny][cur.year];
-                    dq.offer(new Node(nx, ny, cur.year - 1));
-                }
-            }
+            BfsMonkey(cur);
         }
+
+
     }
 
+    private int BfsHorse(Node cur) {
+        for (int i = 0; i < 8; i++) {
+            int nx = cur.x + dxH[i];
+            int ny = cur.y + dyH[i];
 
+            if (nx == r - 1 && ny == c - 1) {
+                return dist[cur.cnt][cur.x][cur.y] + 1;
+            }
+
+            if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
+                continue;
+            }
+
+            if (board[nx][ny] == 1 || dist[cur.cnt + 1][nx][ny] != -1) {
+                continue;
+            }
+
+            dist[cur.cnt + 1][nx][ny] = dist[cur.cnt][cur.x][cur.y] + 1;
+            queue.offer(new Node(nx, ny, cur.cnt+1));
+
+        }
+        return 0;
+    }
+
+    private void BfsMonkey(Node cur) {
+        for (int i = 0; i < 4; i++) {
+            int nx = cur.x + dx[i];
+            int ny = cur.y + dy[i];
+
+            if (nx == r - 1 && ny == c - 1) {
+                System.out.println(dist[cur.cnt][cur.x][cur.y] + 1);
+                System.exit(0);
+            }
+
+            if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
+                continue;
+            }
+            if (board[nx][ny] == 1 || dist[cur.cnt][nx][ny] != -1) {
+                continue;
+            }
+            queue.offer(new Node(nx, ny, cur.cnt));
+            dist[cur.cnt][nx][ny] = dist[cur.cnt][cur.x][cur.y] + 1;
+        }
+    }
 
 
     static class Node{
         int x;
         int y;
-        int year;
+        int cnt;
 
-        public Node(int x, int y, int year) {
+        public Node(int x, int y, int cnt) {
             this.x = x;
             this.y = y;
-            this.year = year;
+            this.cnt = cnt;
         }
+
     }
 }
