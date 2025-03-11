b51a6adf7f228b2f3334108c4772d00a14ca6e53
song hyeongyu <lit3207@ajou.ac.kr>
2025-03-11 17:11:19 +0900

    feat : add Boj1759 solution

--- javaBoj/boj/src/backTracking/Boj1759.java
+++ javaBoj/boj/src/backTracking/Boj1759.java
@@ -11,6 +11,9 @@ public class Boj1759 {
 
     static char[] password;
     static char[] arr;
+    static boolean[] used;
+    static List<Character> mo;
+
 
     static StringBuilder sb = new StringBuilder();
 
@@ -19,9 +22,10 @@ public class Boj1759 {
         process.run();
     }
 
+
     private void run() throws IOException {
         init();
-        func(0,0);
+        func(0, 0);
         System.out.println(sb.toString());
     }
 
@@ -29,11 +33,17 @@ public class Boj1759 {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
+        mo = new ArrayList<>(5);
+        mo.add('a');
+        mo.add('e');
+        mo.add('i');
+        mo.add('o');
+        mo.add('u');
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         password = new char[C];
         arr = new char[C];
+        used = new boolean[C];
         st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < C; i++) {
             password[i] = st.nextToken().charAt(0);
@@ -41,20 +51,46 @@ public class Boj1759 {
         Arrays.sort(password);
     }
 
-    private void func(int count,int cur) {
-        if (count == L) {
+    private void func(int cur, int st) {
+        if (cur == L) {
+            List<Character> lst = new ArrayList<>();
+            int mCnt = 0;
+            int jCnt = 0;
             for (int i = 0; i < L; i++) {
-                sb.append(arr[i]);
+                lst.add(arr[i]);
+            }
+            if (lst.size() < 3) {
+                return;
+            }
+
+            for (Character c : lst) {
+                if (mo.contains(c)) {
+                    mCnt++;
+                }else {
+                    jCnt++;
+                }
+            }
+            if (mCnt < 1 || jCnt < 2) {
+                return;
+            }
+            for (Character c : lst) {
+                sb.append(c);
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = cur; i < C; i++) {
-            arr[count] = password[i];
-            func(count+1,i+1);
-        }
+        char tmp = ' ';
 
+        for (int i = st; i < C; i++) {
+            if (tmp != password[i] && !used[i]) {
+                arr[cur] = password[i];
+                tmp = arr[cur];
+                used[i] = true;
+                func(cur + 1, i);
+                used[i] = false;
+            }
+        }
     }
 
 }
