ee0f1a4d2f987f08e6a95a8cac5aa6c609a202c8
song hyeongyu <lit3207@ajou.ac.kr>
2025-03-11 10:43:06 +0900

    fix : update Boj1922

--- javaBoj/boj/src/recursion/Boj1922.java
+++ javaBoj/boj/src/recursion/Boj1922.java
@@ -6,15 +6,16 @@ import java.io.*;
 
 public class Boj1922 {
     static int N;
-    static int[][] movie;
+    static char[][] board;
     static StringBuilder sb = new StringBuilder();
+
 
     public static void main(String[] args) throws IOException{
         Boj1922 process = new Boj1922();
         process.run();
     }
 
-    private void run() throws IOException{
+    private void run() throws IOException {
         init();
         recursion(0, 0, N);
         System.out.println(sb.toString());
@@ -22,32 +23,27 @@ public class Boj1922 {
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(bf.readLine());
-        movie = new int[N][N];
+        board = new char[N][N];
         for (int i = 0; i < N; i++) {
             String input = bf.readLine();
             for (int j = 0; j < N; j++) {
-                movie[i][j] = input.charAt(j);
+                board[i][j] = input.charAt(j);
             }
         }
 
     }
 
     private void recursion(int x, int y, int n) {
-
         if (isSame(x, y, n)) {
-            int first = movie[x][y];
-
-            if (first == '1') {
-                sb.append("1");
-            }
-            else{
-                sb.append("0");
-            }
+            char fist = board[x][y];
+//            sb.append("(");
+            sb.append(fist);
             return;
         }
-        int half = n / 2;
         sb.append("(");
+        int half = n / 2;
         recursion(x, y, n / 2);
         recursion(x, y + half, n / 2);
         recursion(x + half, y, n / 2);
@@ -56,10 +52,11 @@ public class Boj1922 {
     }
 
     private boolean isSame(int x, int y, int n) {
-        int first = movie[x][y];
-        for (int i = x; i < x+n; i++) {
+        int first = board[x][y];
+
+        for (int i = x; i < x + n; i++) {
             for (int j = y; j < y + n; j++) {
-                if (first != movie[i][j]) {
+                if (first != board[i][j]) {
                     return false;
                 }
             }
