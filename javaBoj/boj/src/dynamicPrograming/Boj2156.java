94ec98aa495f099ab184026bf4b8259bfc0b2ee2
song hyeongyu <lit3207@ajou.ac.kr>
2025-03-19 14:31:44 +0900

    feat : add Boj2156 solution

--- javaBoj/boj/src/dynamicPrograming/Boj2156.java
+++ javaBoj/boj/src/dynamicPrograming/Boj2156.java
@@ -1,38 +1,63 @@
 package dynamicPrograming;
 
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Boj2156 {
     static int N;
     static int[] wine;
-    static int[][] dp;
+    static int[][] d;
 
     public static void main(String[] args) throws IOException{
         Boj2156 process = new Boj2156();
         process.run();
     }
 
-    private void run() throws IOException{
+    private void run() throws IOException {
         init();
-        makeDp();
+        dp();
+        findMaxValue();
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
         N = Integer.parseInt(bf.readLine());
-
-        wine = new int[N+1];
-        dp = new int[N+1][3];
-
+        wine = new int[Math.max(N + 1,3)];
+        d = new int[Math.max(N + 1,3)][3];
         for (int i = 1; i <= N; i++) {
-            wine[i] = Integer.parseInt(bf.readLine());
+            int num = Integer.parseInt(bf.readLine());
+            wine[i] = num;
+        }
+
+
+    }
+
+    private void dp() {
+        d[1][1] = wine[1];
+        d[2][1] = wine[2];
+        d[1][2] = 0;
+        d[2][2] = wine[1] + wine[2];
+
+
+        for (int i = 3; i <= N; i++) {
+            d[i][1] = Math.max(Math.max(d[i - 2][2],d[i-2][1]), d[i - 3][2]) + wine[i];
+            d[i][2] = d[i - 1][1] + wine[i];
         }
     }
 
-    private void makeDp() {
 
+    private void findMaxValue() {
+        int maxVale = Integer.MIN_VALUE;
+
+        for (int[] m : d) {
+            maxVale = Math.max(maxVale,Arrays.stream(m).max().orElse(0));
+        }
+
+        System.out.println(maxVale);
     }
-
 }
+
+
+
