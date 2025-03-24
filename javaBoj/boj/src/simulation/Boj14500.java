e9eb9b83d34d7a2c3c06bdce344c4025c6804c2c
song hyeongyu <lit3207@ajou.ac.kr>
2025-03-24 11:23:05 +0900

    feat : add Boj14500 solution

---
+++ javaBoj/boj/src/simulation/Boj14500.java
@@ -0,0 +1,25 @@
+package simulation;
+
+import java.util.*;
+import java.io.*;
+
+public class Boj14500 {
+    static int N;
+    static int M;
+    static int count;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    static char[][] board;
+
+
+    public static void main(String[] args) throws IOException{
+        Boj14500 process = new Boj14500();
+        process.run();
+    }
+
+    private void run() throws IOException {
+    }
+
+
+}
