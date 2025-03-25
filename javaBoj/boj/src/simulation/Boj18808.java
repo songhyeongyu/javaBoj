796df044adf6fcbedbcc370042cd50f00358a4c6
song hyeongyu <lit3207@ajou.ac.kr>
2025-03-25 10:40:30 +0900

    feat : try to solve Boj18808

--- javaBoj/boj/src/simulation/Boj18808.java
+++ javaBoj/boj/src/simulation/Boj18808.java
@@ -8,14 +8,14 @@ public class Boj18808 {
     /**
      * 스티커를 회전 x
      * 스티커 붙일 수 있는 위치 찾음 -> 여러곳이 있으면 위쪽 왼쪽부터 -> 위쪽 오른쪽까지
-     * 위치에 붙이고 -> 위치가 없으면 -> 90도로 2번 회전(시계방향)
      */
 
     static int r;
     static int c;
     static int s;
-    static int[][][] sticker;
     static int[][] board;
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    List<int[][]> stickerSet = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         Boj18808 process = new Boj18808();
@@ -24,107 +24,54 @@ public class Boj18808 {
 
     private void run() throws IOException{
         init();
+
+        System.out.println(Arrays.deepToString(board));
     }
 
     private void init() throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(bf.readLine());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
         s = Integer.parseInt(st.nextToken());
 
         board = new int[r][c];
-        sticker = new int[s][r][c];
+
         for (int i = 0; i < s; i++) {
             st = new StringTokenizer(bf.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            sticker[i] = new int[n][m];
-
-            for (int j = 0; j < n; j++) {
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int[][] sticker = new int[r][c];
+            for (int j = 0; j < r; j++) {
                 st = new StringTokenizer(bf.readLine());
-                for (int k = 0; k < m; k++) {
-                    sticker[i][j][k] = Integer.parseInt(st.nextToken());
+                for (int k = 0; k < c; k++) {
+                    sticker[j][k] = Integer.parseInt(st.nextToken());
                 }
             }
-        }
-
-    }
-
-    /*
-1 3 3
-2 3
-1 0 0
-1 1 1
-1 1
-1
-3 1
-1
-1
-1
-     */
-
-    /**
-     * 남은 공간 산을 해야 되나? 어디가 남았는지 ?
-     */
-    private void paste() {
-        for (int i = 0; i < s; i++) {
-            if (isPaste(sticker[i])) {
-                changeBoard(sticker[i]);
+            stickerSet.add(sticker);
             }
-            else{
 
-            }
-        }
-    }
 
-    private void moveSticker(int[][] arr) {
 
     }
 
-    private void changeBoard(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                board[i][j] = arr[i][j];
+
+    private int[][] turnRight(int x, int y,int[][] sticker) {
+        int temp = x;
+        x = y;
+        y = temp;
+        int[][] tempArr = new int[x][y];
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                tempArr[i][(y - 1) - j] = sticker[j][i];
             }
         }
 
+        return tempArr;
     }
 
-    private boolean isPaste(int[][] arr) {
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (board[i][j] != 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 
-    private int[][] turnRight(int i, int n, int m) {
-        int[][] rotated = new int[m][n];
 
-        for (int j = 0; j < m; j++) {
-            for (int k = 0; k < n; k++) {
-                rotated[j][k] = sticker[i][n - 1 - k][j];
-            }
-        }
 
-        return rotated;
-    }
-
-    private int[][] turn(int i, int n, int m) {
-        int[][] rotate = new int[n][m];
-
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < m; k++) {
-                rotate[j][k] = sticker[i][(n-1) - j][k];
-            }
-        }
-
-        return rotate;
-    }
 
 }
