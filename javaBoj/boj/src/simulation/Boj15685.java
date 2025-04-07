15b4fcaac60d0a97726951a2203145b2091c158e
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-07 11:21:02 +0900

    feat : add Boj15685 solution

--- javaBoj/boj/src/simulation/Boj15685.java
+++ javaBoj/boj/src/simulation/Boj15685.java
@@ -4,12 +4,12 @@ import java.util.*;
 import java.io.*;
 
 public class Boj15685 {
+
     static int N;
-    static Set<Node> point;
-    static List<Integer> direction;
-    static List<Node> lst;
+
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, -1, 0, 1};
+    static boolean[][] board;
 
     public static void main(String[] args) throws IOException{
         Boj15685 process = new Boj15685();
@@ -18,63 +18,57 @@ public class Boj15685 {
 
     private void run() throws IOException {
         init();
-
+        System.out.println(countSquare());
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        //  방향을 고정한 상태에서 똑같은 방향 + 움직임
         N = Integer.parseInt(bf.readLine());
-        point = new HashSet<>();
-        direction = new ArrayList<>();
-        lst = new ArrayList<>();
+        board = new boolean[102][102];
+
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
+
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
             int g = Integer.parseInt(st.nextToken());
-            point.add(new Node(x, y));
-            lst.add(new Node(x, y));
-            makePoint(x, y, d, g);
 
-
-
+            makeDragon(x, y, d, g);
         }
     }
 
-    private void makePoint(int x, int y, int d, int g) {
-        point.add(new Node(x+dx[d], y+dy[d]));
-        lst.add(new Node(x+dx[d], y+dy[d]));
+    private void makeDragon(int x, int y, int d, int g) {
+        List<Integer> dragons = new ArrayList<>();
 
-        for (int i = 1; i <= g; i++) {
-            d = (d + 1) % 4;
+        board[x][y] = true;
+        dragons.add(d);
 
-            for (int j = 0; j < i; j++) {
-
-                int r = lst.getLast().x;
-                int c = lst.getLast().y;
-
-                point.add(new Node(r + dx[(d+j)%4], c + dy[(d+j)%4]));
-                lst.add(new Node(r + dx[(d+j)%4], c + dy[(d+j)%4]));
+        for (int i = 0; i < g; i++) {
+            for (int j = dragons.size() - 1; j > -1; j--) {
+                int cur = dragons.get(j);
+                int nd = (cur + 1) % 4;
+                dragons.add(nd);
             }
+        }
 
-            for (Node node : lst) {
-                System.out.println(node.x + " " + node.y);
+        for (Integer dragon : dragons) {
+            x += dx[dragon];
+            y += dy[dragon];
+            board[x][y] = true;
+        }
+    }
+
+    private int countSquare() {
+        int count = 0;
+        for (int i = 0; i < 101; i++) {
+            for (int j = 0; j < 101; j++) {
+                if (board[i][j] && board[i + 1][j] && board[i + 1][j + 1] && board[i][j + 1]) {
+                    count++;
+                }
             }
-            System.out.println("-".repeat(20));
-
-
         }
+        return count;
     }
 
-    static class Node{
-        int x;
-        int y;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
 }
