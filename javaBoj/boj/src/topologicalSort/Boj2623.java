036fa49a49bbab877d847c5abd95e1150277fb4a
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-11 10:25:52 +0900

    feat : add Boj2623 solution

--- javaBoj/boj/src/topologicalSort/Boj2623.java
+++ javaBoj/boj/src/topologicalSort/Boj2623.java
@@ -7,9 +7,9 @@ public class Boj2623 {
     static int N;
     static int M;
     static int[] indegree;
-    static Queue<Integer> queue;
     static List<Integer> result;
     static List<List<Integer>> graph;
+
     public static void main(String[] args) throws IOException {
         Boj2623 process = new Boj2623();
         process.run();
@@ -17,40 +17,47 @@ public class Boj2623 {
 
     private void run() throws IOException {
         init();
-        sort();
+        topoSort();
+        printResult();
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
+
         indegree = new int[N + 1];
+
         graph = new ArrayList<>();
-        queue = new LinkedList<>();
         result = new ArrayList<>();
+
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(bf.readLine());
-            int t = Integer.parseInt(st.nextToken());
+
+            int k = Integer.parseInt(st.nextToken());
+
             int first = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < t-1; j++) {
+            for (int j = 0; j < k - 1; j++) {
                 int second = Integer.parseInt(st.nextToken());
                 graph.get(first).add(second);
-                first = second;
                 indegree[second]++;
+                first = second;
             }
         }
 
     }
 
-    private void sort() {
-        for (int i = 1 ; i <=N; i++) {
+    private void topoSort() {
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
             if (indegree[i] == 0) {
                 queue.offer(i);
             }
@@ -66,14 +73,21 @@ public class Boj2623 {
                         queue.offer(nxt);
                     }
                 }
-            }
 
+            }
             result.add(cur);
         }
 
+
+
+    }
+
+    private void printResult() {
         if (result.size() != N) {
             System.out.println(0);
+            return;
         }
+
         for (Integer integer : result) {
             System.out.println(integer);
         }
