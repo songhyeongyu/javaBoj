2288a092958fa41faf1ef9a235aafca9d5b9f5d5
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-11 10:06:24 +0900

    feat : add Boj2252 solution

--- javaBoj/boj/src/topologicalSort/Boj2252.java
+++ javaBoj/boj/src/topologicalSort/Boj2252.java
@@ -1,65 +1,81 @@
 package topologicalSort;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Boj2252 {
     static int N;
     static int M;
-    static List<Integer>[] graph;
-    static int[] deg;
+
+    static List<List<Integer>> graph;
+    static List<Integer> result;
+    static int[] indegree;
 
     public static void main(String[] args) throws IOException {
         Boj2252 process = new Boj2252();
         process.run();
     }
 
-    private void run() throws IOException{
+    private void run() throws IOException {
         init();
-        findTopological();
+        topoSort();
+        printResult();
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        deg = new int[N + 1];
-        graph = new ArrayList[N + 1];
 
-        for (int i = 0; i < N + 1; i++) {
-            graph[i] = new ArrayList<>();
+        indegree = new int[N + 1];
+        graph = new ArrayList<>();
+        result = new ArrayList<>();
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(bf.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            deg[v] += 1;
-            graph[u].add(v);
+            graph.get(u).add(v);
+            indegree[v]++;
         }
+
     }
 
+    private void topoSort() {
+        Queue<Integer> queue = new LinkedList<>();
 
-    private void findTopological() {
-        Queue<Integer> que = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
-            if (deg[i] == 0) {
-                que.offer(i);
+            if (indegree[i]==0) {
+                queue.offer(i);
             }
         }
-            while (!que.isEmpty()) {
-                int cur = que.poll();
-                System.out.print(cur + " ");
-                for (int nxt : graph[cur]) {
-                    deg[nxt]--;
-                    if (deg[nxt] == 0) {
-                        que.offer(nxt);
+
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            for (int nxt : graph.get(cur)) {
+                if (indegree[nxt] > 0) {
+                    indegree[nxt]--;
+                    if (indegree[nxt] == 0) {
+                        queue.offer(nxt);
                     }
                 }
             }
+            result.add(cur);
+        }
+
+    }
+
+    private void printResult() {
+        for (Integer integer : result) {
+            System.out.print(integer + " ");
+        }
     }
 }
