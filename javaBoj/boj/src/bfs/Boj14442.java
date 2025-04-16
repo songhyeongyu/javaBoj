a1f9f0528a21d57c0dc62aa75d6aacd494e570ae
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-16 17:34:39 +0900

    feat : add Boj14442 solution

--- javaBoj/boj/src/bfs/Boj14442.java
+++ javaBoj/boj/src/bfs/Boj14442.java
@@ -3,14 +3,18 @@ package bfs;
 import java.util.*;
 import java.io.*;
 
-
 public class Boj14442 {
-    static int N, M, K;
-    static char[][][] board;
-    static int[][][] dist;
+    static int N;
+    static int M;
+    static int K;
 
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
+
+    static char[][] board;
+    static int[][][] dist;
+
+
 
     public static void main(String[] args) throws IOException{
         Boj14442 process = new Boj14442();
@@ -19,46 +23,44 @@ public class Boj14442 {
 
     private void run() throws IOException {
         init();
-        System.out.println(bfs(0, 0));
+        System.out.println(bfs());
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        board = new char[N][M][K+1];
-        dist = new int[N][M][K+1];
+        board = new char[N][M];
+        dist = new int[N][M][K + 1];
 
         for (int i = 0; i < N; i++) {
-            String input = bf.readLine();
+            String s = bf.readLine();
             for (int j = 0; j < M; j++) {
-                board[i][j][0] = input.charAt(j);
-                for (int k = 0; k < K; k++) {
+                board[i][j] = s.charAt(j);
+                for (int k = 0; k <= K; k++) {
                     dist[i][j][k] = -1;
                 }
             }
         }
     }
 
-    private int bfs(int x, int y) {
+    private int bfs() {
         Queue<Node> que = new LinkedList<>();
-        que.offer(new Node(x, y, 0));
-        for (int i = 0; i < K+1; i++) {
-            dist[0][0][i] = 0;
-        }
+        dist[0][0][0] = 1;
+        que.offer(new Node(0, 0, 0));
 
         while (!que.isEmpty()) {
             Node cur = que.poll();
-            int nxt = dist[cur.x][cur.x][cur.broken] + 1;
+            int nxtDist = dist[cur.x][cur.y][cur.broken] + 1;
 
             if (cur.x == N - 1 && cur.y == M - 1) {
-                return dist[cur.x][cur.y][cur.broken] + 1;
+                return dist[cur.x][cur.y][cur.broken];
             }
-
             for (int i = 0; i < 4; i++) {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
@@ -67,27 +69,22 @@ public class Boj14442 {
                     continue;
                 }
 
-                if (dist[nx][ny][cur.broken] == -1 && board[nx][ny][cur.broken] == '0') {
-                    dist[nx][ny][cur.broken] = nxt;
-                    que.add(new Node(nx, ny, cur.broken));
+                if (dist[nx][ny][cur.broken] == -1 && board[nx][ny] == '0') {
+                    dist[nx][ny][cur.broken] = nxtDist;
+                    que.offer(new Node(nx, ny, cur.broken));
                 }
 
-                if (dist[nx][ny][cur.broken] == -1 && board[nx][ny][cur.broken] == '1' && cur.broken < K) {
-                    if (cur.broken == K) {
-                        dist[nx][ny][cur.broken] = nxt;
-                        que.add(new Node(nx, ny, cur.broken));
-                        continue;
-                    }
-                    dist[nx][ny][cur.broken++] = nxt;
-                    que.add(new Node(nx, ny, cur.broken++));
+                if (cur.broken < K && dist[nx][ny][cur.broken+1] == -1 && board[nx][ny] == '1') {
+                    dist[nx][ny][cur.broken + 1] = nxtDist;
+                    que.offer(new Node(nx, ny, cur.broken + 1));
                 }
             }
         }
-
         return -1;
     }
 
-    static class Node {
+
+    static class Node{
         int x;
         int y;
         int broken;
@@ -98,4 +95,5 @@ public class Boj14442 {
             this.broken = broken;
         }
     }
+
 }
