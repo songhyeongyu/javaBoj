26d7dd4560fc5ca4e1794768ce49a0b20164ac82
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-16 16:47:55 +0900

    feat : add Boj1600 solution

--- javaBoj/boj/src/bfs/Boj1600.java
+++ javaBoj/boj/src/bfs/Boj1600.java
@@ -1,121 +1,117 @@
 package bfs;
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Boj1600 {
+    static int T;
+    static int N;
+    static int M;
+    static char[][] board;
+    static int[][][] dist;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
 
-    static int[] dxH = {2, 1, -1, -2, -2, -1, 1, -2};
-    static int[] dyH = {1, 2, 2, 1, -1, -2, -2, -1};
 
-    static int k;
-    static int r;
-    static int c;
+    static int[] dxH = {2, 1, -1, -2, -2, -1, 2, 1};
+    static int[] dyH = {1, 2, 2, 1, -1, -2, -1, -2};
 
-    static int[][] board;
-    static int[][][] dist;
-    static Queue<Node> queue = new LinkedList<>();
-    public static void main(String[] args) throws IOException{
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
         Boj1600 process = new Boj1600();
         process.run();
     }
 
     private void run() throws IOException {
         init();
-        bfs(0, 0);
+        System.out.println(makeDistance());
+//        printDist();
 
     }
-    /*2
-10 2
-0 0 1 0 0 1 0 0 1 0
-0 0 1 1 0 0 0 0 1 0*/
-    private void init() throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        k = Integer.parseInt(bf.readLine());
+
+    private void init() throws IOException {
+        T = Integer.parseInt(bf.readLine());
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        c = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        board = new int[r][c];
-        dist = new int[32][r][c];
-        for (int i = 0; i < r; i++) {
+        board = new char[N][M];
+        dist = new int[N][M][T+1];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < c; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                dist[0][i][j] = -1;
+            for (int j = 0; j < M; j++) {
+                board[i][j] = st.nextToken().charAt(0);
+                for (int k = 0; k <= T; k++) {
+                    dist[i][j][k] = -1;
+                }
             }
         }
-
-        for (int z = 0; z <= k; z++) {
-            for (int i = 0; i < r; i++) {
-                Arrays.fill(dist[z][i], -1);
-            }
-        }
-
     }
 
-    private void bfs(int x, int y) {
-        queue.offer(new Node(x, y,0));
-        dist[0][x][y] = 0;
+    private int makeDistance() {
+        Queue<Node> que = new LinkedList<>();
+        que.offer(new Node(0, 0, 0));
+        dist[0][0][0] = 0;
 
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
+        while (!que.isEmpty()) {
+            Node cur = que.poll();
+            int nextDist = dist[cur.x][cur.y][cur.jump] + 1;
 
-
-            if (cur.cnt < k) {
-                BfsHorse(cur);
+            //1. 원숭이 점프
+            if (cur.x == N - 1 && cur.y == M - 1) {
+                return dist[cur.x][cur.y][cur.jump];
             }
 
-            BfsMonkey(cur);
+            if(cur.jump+1 <= T){
+                for (int i = 0; i < 8; i++) {
+                    int nx = cur.x + dxH[i];
+                    int ny = cur.y + dyH[i];
+
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                        continue;
+                    }
+
+                    if (board[nx][ny] == '1' || dist[nx][ny][cur.jump + 1] != -1) {
+                        continue;
+                    }
+                    dist[nx][ny][cur.jump + 1] = nextDist;
+                    que.offer(new Node(nx, ny, cur.jump + 1));
+                }
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                    continue;
+                }
+
+                if (board[nx][ny] == '1' || dist[nx][ny][cur.jump] != -1) {
+                    continue;
+                }
+                dist[nx][ny][cur.jump] = nextDist;
+                que.offer(new Node(nx, ny, cur.jump));
+            }
+
+
         }
-
-
+        return -1;
     }
 
-    private int BfsHorse(Node cur) {
-        for (int i = 0; i < 8; i++) {
-            int nx = cur.x + dxH[i];
-            int ny = cur.y + dyH[i];
-
-            if (nx == r - 1 && ny == c - 1) {
-                return dist[cur.cnt][cur.x][cur.y] + 1;
+    private void printDist() {
+        for (int k = 0; k <= T; k++) {
+            System.out.println("Jump Level: " + k);
+            for (int i = 0; i < N; i++) {
+                int[] row = new int[M];
+                for (int j = 0; j < M; j++) {
+                    row[j] = dist[i][j][k];
+                }
+                System.out.println(Arrays.toString(row));
             }
-
-            if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
-                continue;
-            }
-
-            if (board[nx][ny] == 1 || dist[cur.cnt + 1][nx][ny] != -1) {
-                continue;
-            }
-
-            dist[cur.cnt + 1][nx][ny] = dist[cur.cnt][cur.x][cur.y] + 1;
-            queue.offer(new Node(nx, ny, cur.cnt+1));
-
-        }
-        return 0;
-    }
-
-    private void BfsMonkey(Node cur) {
-        for (int i = 0; i < 4; i++) {
-            int nx = cur.x + dx[i];
-            int ny = cur.y + dy[i];
-
-            if (nx == r - 1 && ny == c - 1) {
-                System.out.println(dist[cur.cnt][cur.x][cur.y] + 1);
-                System.exit(0);
-            }
-
-            if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
-                continue;
-            }
-            if (board[nx][ny] == 1 || dist[cur.cnt][nx][ny] != -1) {
-                continue;
-            }
-            queue.offer(new Node(nx, ny, cur.cnt));
-            dist[cur.cnt][nx][ny] = dist[cur.cnt][cur.x][cur.y] + 1;
+            System.out.println("-".repeat(30));
         }
     }
 
@@ -123,13 +119,14 @@ public class Boj1600 {
     static class Node{
         int x;
         int y;
-        int cnt;
+        int jump;
 
-        public Node(int x, int y, int cnt) {
+        public Node(int x, int y, int jump) {
             this.x = x;
             this.y = y;
-            this.cnt = cnt;
+            this.jump = jump;
         }
-
     }
+
+
 }
