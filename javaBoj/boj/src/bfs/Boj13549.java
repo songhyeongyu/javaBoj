8156225c5edb5194531aa096bcb11d81ff888491
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-28 13:22:06 +0900

    feat : add Boj13549 solution

--- javaBoj/boj/src/bfs/Boj13549.java
+++ javaBoj/boj/src/bfs/Boj13549.java
@@ -3,74 +3,68 @@ package bfs;
 import java.util.*;
 import java.io.*;
 
-
 public class Boj13549 {
-
     static int N;
-    static int K;
-    static int[] arr;
-    static boolean[] visit;
-    static Deque<Integer> dq = new LinkedList<>();
+    static int M;
+
+    static int[] board;
 
     public static void main(String[] args) throws IOException{
-        new Boj13549().run();
+        Boj13549 process = new Boj13549();
+        process.run();
     }
 
     private void run() throws IOException{
         init();
+        bfs();
+//        System.out.println(Arrays.toString(board));
+        System.out.println(board[M]);
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        swap();
-        arr = new int[K + 2];
-        visit = new boolean[K + 2];
-        bfs();
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(visit));
+        M = Integer.parseInt(st.nextToken());
+//        if (N > M) {
+//            int temp = N;
+//            N = M;
+//            M = temp;
+//        }
 
+        board = new int[Math.max(N, M) * 2 + 2];
 
+        Arrays.fill(board,-1);
     }
 
     private void bfs() {
+        Deque<Integer> dq = new LinkedList<>();
         dq.offer(N);
-        visit[N] = true;
+        board[N] = 0;
+
+
         while (!dq.isEmpty()) {
-            Integer cur = dq.pollFirst();
+            int cur = dq.pollFirst();
+            if (cur == M) {
+                break;
+            }
+            if (cur * 2 < board.length && board[cur * 2] == -1) {
+                board[cur * 2] = board[cur];
+                dq.offerFirst(cur * 2);
+            }
 
-            if (cur > 0 && cur < K + 1) {
-                int now = 2 * cur;
-                if (!(now < 0 || now > K + 1 || visit[now])) {
-                    arr[2 * cur] = 0;
-                    dq.offerFirst(2 * cur);
-                    visit[2 * cur] = true;
+            int[] dx = {cur - 1, cur + 1};
+            for (int i = 0; i < 2; i++) {
+                int nx = dx[i];
+                if (nx < 0 || nx >= board.length) {
+                    continue;
                 }
-
-
-                int[] dir = {cur - 1, cur + 1};
-
-                for (int i = 0; i < 2; i++) {
-                    int now1 = dir[i];
-                    if (now1 < 0 || now1 >= K) {
-                        continue;
-                    }
-                    arr[now1] = arr[cur] + 1;
-                    visit[now1] = true;
-                    dq.offerLast(now1);
+                if (board[nx] == -1) {
+                    board[nx] = board[cur] + 1;
+                    dq.offer(nx);
                 }
             }
         }
-
     }
-    private static void swap() {
-        if (N > K) {
-            int temp = N;
-            N = K;
-            K = temp;
-        }
-    }
-
 }
