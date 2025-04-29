0ff258a47cd1809ce81a1dda53c646d4ca78a093
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-29 15:22:35 +0900

    feat : add Boj9251 solution

--- javaBoj/boj/src/dynamicPrograming/Boj9251.java
+++ javaBoj/boj/src/dynamicPrograming/Boj9251.java
@@ -4,49 +4,49 @@ import java.util.*;
 import java.io.*;
 
 public class Boj9251 {
-    static char[] str1;
-    static char[] str2;
+    static char[] src;
+    static char[] dst;
+    static int[][] dp;
 
-    static int[][] board;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         Boj9251 process = new Boj9251();
         process.run();
     }
 
     private void run() throws IOException {
         init();
-        dp();
-        System.out.println(board[str1.length -1][str1.length-1]);
+        dynamic();
+        System.out.println(dp[src.length-1][dst.length-1]);
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String s1 = bf.readLine();
-        String s2 = bf.readLine();
-        board = new int[s1.length()+1][s1.length()+1];
-        str1 = new char[s1.length()+1];
-        str2 = new char[s2.length()+1];
-        for (int i = 1; i <= s1.length(); i++) {
-            str1[i] = s1.charAt(i-1);
+        String str1 = bf.readLine();
+        String str2 = bf.readLine();
+        src = new char[str1.length()+1];
+        dst = new char[str2.length()+1];
+        for (int i = 1; i <= str1.length(); i++) {
+            src[i] = str1.charAt(i-1);
         }
 
-        for (int i = 1; i <= s2.length(); i++) {
-            str2[i] = s2.charAt(i-1);
+        for (int i = 1; i <= str2.length(); i++) {
+            dst[i] = str2.charAt(i-1);
         }
+        dp = new int[str1.length() + 1][str2.length() + 1];
     }
 
-    private void dp() {
+    private void dynamic() {
 
-        for (int i = 1; i < str1.length; i++) {
-            for (int j = 1; j < str1.length; j++) {
-                if (str1[i] == str2[j]) {
-                    board[i][j] = board[i - 1][j - 1] + 1;
-                }else{
-                    board[i][j] = Math.max(board[i - 1][j], board[i][j - 1]);
+        for (int i = 1; i < src.length; i++) {
+            for (int j = 1; j < dst.length; j++) {
+                if (src[i] == dst[j]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    continue;
                 }
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
+
     }
 }
