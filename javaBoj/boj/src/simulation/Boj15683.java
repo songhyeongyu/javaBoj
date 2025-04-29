bff61008caebd604ebed96eddc82e1a9da61c14a
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-29 10:16:10 +0900

    fix : update Boj15683 solution

--- javaBoj/boj/src/simulation/Boj15683.java
+++ javaBoj/boj/src/simulation/Boj15683.java
@@ -3,139 +3,122 @@ package simulation;
 import java.util.*;
 import java.io.*;
 
-
 public class Boj15683 {
-    static int row;
-    static int col;
-    static int answer = Integer.MAX_VALUE;
+    static int N;
+    static int M;
 
-    public static void main(String[] args) throws IOException {
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+
+    static int[][] board;
+
+    static int[][][] dir = {
+            {},
+            {{0}, {1}, {2}, {3}},
+            {{0, 1}, {2, 3}},
+            {{0, 2}, {0, 3}, {1, 2}, {1, 3}},
+            {{0, 1, 2}, {0, 2, 3}, {3, 0, 1}, {1, 2, 3}},
+            {{0, 1, 2, 3}}
+    };
+
+    static int minValue = Integer.MAX_VALUE;
+    static List<Node> cctv = new ArrayList<>();
+
+
+
+    public static void main(String[] args) throws IOException{
         Boj15683 process = new Boj15683();
         process.run();
     }
 
-    public void run() throws IOException {
+    private void run() throws IOException {
         init();
-        System.out.println(answer);
-
-
+        recur(0);
+        System.out.println(minValue);
     }
 
-
-    public void init() throws IOException {
+    private void init() throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        row = Integer.parseInt(st.nextToken());
-        col = Integer.parseInt(st.nextToken());
-        int[][] board = new int[row][col];
-        List<Node> cctv = new LinkedList<>();
-        for (int i = 0; i < row; i++) {
-            StringTokenizer input = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < col; j++) {
-                int number = Integer.parseInt(input.nextToken());
-                if (number != 0 && number != 6) {
-                    cctv.add(new Node(i, j, number, number + i + j));
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < M; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num > 0 && num < 6) {
+                    cctv.add(new Node(i, j, num));
                 }
-                board[i][j] = number;
+                board[i][j] = num;
             }
         }
-        recur(cctv,0,board);
-
     }
 
-    private void recur(List<Node> cctv, int index, int[][] map) {
-        if (index >= cctv.size()) {
+    private void recur(int index) {
+        if (index == cctv.size()) {
             int count = 0;
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[0].length; j++) {
-                    if (map[i][j] == 0) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (board[i][j] == 0) {
                         count++;
                     }
                 }
             }
-            answer = Math.min(answer, count);
+            minValue = Math.min(count, minValue);
             return;
         }
+
         Node cur = cctv.get(index);
 
-        for (int i = 0; i < 4; i++) {
-            checkCctv(i,cur.r,cur.c,cur.cctvNum, cur.cctvDifNum, map);
-            recur(cctv, index + 1, map);
-            checkCctv(i,cur.r,cur.c,cur.cctvNum, cur.cctvDifNum, map);
-        }
-    }
+        int[][] direction = dir[cur.num];
 
-    private void checkCctv(int dir, int r, int c, int cctvNum, int cctvDif, int[][] map) {
-        if (cctvNum == 1) {
-            check(r, c, map, dir, cctvDif);
-        } else if (cctvNum == 2) {
-            // 2번 CCTV: 서로 반대 방향 (0-2, 1-3)
-            check(r, c, map, dir, cctvDif);
-            check(r, c, map, (dir + 2) % 4, cctvDif);
-        } else if (cctvNum == 3) {
-            // 3번 CCTV: 직각 방향
-            check(r, c, map, dir, cctvDif);
-            check(r, c, map, (dir + 1) % 4, cctvDif);
-        } else if (cctvNum == 4) {
-            // 4번 CCTV: 3방향 감시
-            check(r, c, map, dir, cctvDif);
-            check(r, c, map, (dir + 1) % 4, cctvDif);
-            check(r, c, map, (dir + 2) % 4, cctvDif);
-        } else if (cctvNum == 5) {
-            // 5번 CCTV: 모든 방향 감시
-            for (int i = 0; i < 4; i++) {
-                check(r, c, map, i, cctvDif);
+        for (int[] d : direction) {
+            List<Node> temp = new ArrayList<>();
+            checkCctv(d,cur.x, cur.y, cur.num, temp);
+            recur(index + 1);
+            for (Node n : temp) {
+                board[n.x][n.y] = 0;
             }
         }
     }
 
-
-
-
-
-    private void check(int r, int c, int[][] map, int dir, int cctvDif) {
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-        while (map[r][c] != 6) {
-            r = dx[dir] + r;
-            c = dy[dir] + c;
-
-            if (r < 0 || r >= row || c < 0 || c >= col) {
-                break;
-            }else {
-                if (map[r][c] == 0) {
-                    map[r][c] = cctvDif;
-                } else if (map[r][c] == cctvDif) {
-                    map[r][c] = 0;
+    private void checkCctv(int[] dir, int x, int y, int num, List<Node> temp) {
+        for (int d : dir) {
+            int nx = x;
+            int ny = y;
+            board[nx][ny] = num;
+            while (true) {
+                nx += dx[d];
+                ny += dy[d];
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                    break;
                 }
+                if (board[nx][ny] == 6) {
+                    break;
+                }
+                if (board[nx][ny] == 0) {
+                    board[nx][ny] = num;
+                    temp.add(new Node(nx, ny, num));
+                }
+
             }
         }
     }
 
+    static class Node{
+        int x;
+        int y;
+        int num;
 
-
-    static class Node {
-        int r;
-        int c;
-        int cctvNum;
-        int cctvDifNum;
-
-        public Node(int r, int c, int cctvNum, int cctvDifNum) {
-            this.r = r;
-            this.c = c;
-            this.cctvNum = cctvNum;
-            this.cctvDifNum = cctvDifNum;
-        }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "r=" + r +
-                    ", c=" + c +
-                    ", cctvNum=" + cctvNum +
-                    ", cctvDifNum=" + cctvDifNum +
-                    '}';
+        public Node(int x, int y, int num) {
+            this.x = x;
+            this.y = y;
+            this.num = num;
         }
     }
 }
