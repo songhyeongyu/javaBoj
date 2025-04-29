d7e944df735596812d550ccb92cc2a54e35e3b26
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-29 11:10:26 +0900

    fix : update Boj18808 solution

--- javaBoj/boj/src/simulation/Boj18808.java
+++ javaBoj/boj/src/simulation/Boj18808.java
@@ -1,58 +1,61 @@
 package simulation;
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Boj18808 {
     static int N;
     static int M;
     static int K;
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     static int[][] board;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         Boj18808 process = new Boj18808();
         process.run();
     }
 
-    private void run() throws IOException{
+    private void run() throws IOException {
         init();
-        System.out.println(countZero());
+        make();
+        int ans = countSticker();
+        System.out.println(ans);
     }
 
     private void init() throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        board = new int[N][M];
 
-        while (K> 0) {
-            K--;
-            st = new StringTokenizer(bf.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int[][] sticker = new int[r][c];
-            for (int i = 0; i < r; i++) {
+        board = new int[N][M];
+    }
+
+    private void make() throws IOException {
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int[][] s = new int[n][m];
+            for (int j = 0; j < n; j++) {
                 st = new StringTokenizer(bf.readLine());
-                for (int j = 0; j < c; j++) {
-                    sticker[i][j] = Integer.parseInt(st.nextToken());
+                for (int k = 0; k < m; k++) {
+                    s[j][k] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            for (int rotate = 0; rotate < 4; rotate++) {
-                int x = sticker.length;
-                int y = sticker[0].length;
+            for (int j = 0; j < 4; j++) {
+                int x = s.length;
+                int y = s[0].length;
                 boolean flag = false;
-                for (int i = 0; i <= N - x; i++) {
+                for (int k = 0; k <= N - x; k++) {
                     if (flag) {
                         break;
                     }
-                    for (int j = 0; j <= M - y; j++) {
-                        if (paste(i, j, sticker)) {
-                            flag = true;
+                    for (int l = 0; l <= M - y; l++) {
+                        flag = isPaste(s, l, k, x, y);
+                        if (flag) {
+                            paste(s, k, l, x, y);
                             break;
                         }
                     }
@@ -61,30 +64,28 @@ public class Boj18808 {
                 if (flag) {
                     break;
                 }
-                sticker = turn(sticker);
-            }
-
-
-        }
-
-    }
-
-    private boolean paste(int r, int c, int[][] sticker) {
-        int x = sticker.length;
-        int y = sticker[0].length;
-
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                if (board[r + i][c + j] == 1 && sticker[i][j] == 1) {
-                    return false;
+                else{
+                    s = turnRight(s);
                 }
             }
         }
+    }
 
+    private void paste(int[][] s, int k, int l, int x, int y) {
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                if (sticker[i][j] == 1) {
-                    board[i + r][j + c] = 1;
+                if(s[i][j] == 1)
+                    board[i + k][j + l] = 1;
+            }
+        }
+    }
+
+
+    private boolean isPaste(int[][] s, int l, int k, int x, int y) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if (s[i][j] == 1 && board[i+k][j+l] == 1) {
+                    return false;
                 }
             }
         }
@@ -92,23 +93,20 @@ public class Boj18808 {
     }
 
 
-    private int[][] turn(int[][] arr) {
-        int r = arr[0].length;
-        int c = arr.length;
-
-        int[][] temp = new int[r][c];
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                temp[i][j] = arr[(c - 1) - j][i];
+    private int[][] turnRight(int[][] board) {
+        int[][] temp = new int[board[0].length][board.length];
+        for (int i = 0; i < board[0].length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                temp[i][j] = board[(board.length - 1) - j][i];
             }
         }
+
+
         return temp;
     }
 
-    private int countZero() {
+    private int countSticker() {
         int count = 0;
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (board[i][j] == 1) {
@@ -116,9 +114,7 @@ public class Boj18808 {
                 }
             }
         }
-
         return count;
     }
-
 
 }
