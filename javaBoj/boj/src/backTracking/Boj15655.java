f6e18b76085780dbcb4b6009e1d794f5c2cb15d9
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-30 09:50:29 +0900

    fix : update Boj15655 solution

--- javaBoj/boj/src/backTracking/Boj15655.java
+++ javaBoj/boj/src/backTracking/Boj15655.java
@@ -3,64 +3,57 @@ package backTracking;
 import java.util.*;
 import java.io.*;
 
-/*조합*/
 public class Boj15655 {
-
     static int N;
     static int M;
     static int[] arr;
-    static int[] ans;
-    static boolean[] isUsed;
+    static int[] temp;
+    static boolean[] isused;
+
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException{
         Boj15655 process = new Boj15655();
-
         process.run();
     }
 
-    public void run() throws IOException{
+    private void run() throws IOException{
         init();
-        func(0);
+        func(0,0);
         System.out.println(sb.toString());
     }
 
-    public void init() throws IOException{
+    private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        StringTokenizer stArr = new StringTokenizer(bf.readLine());
-
-        arr = new int[N+1];
-        ans = new int[10];
-        isUsed = new boolean[10];
+        arr = new int[N];
+        temp = new int[N];
+        isused = new boolean[N];
+        st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(stArr.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(arr);
+
     }
 
-    public void func(int cur) {
+    private void func(int cur, int st) {
         if (cur == M) {
             for (int i = 0; i < M; i++) {
-                sb.append(ans[i]).append(" ");
+                sb.append(arr[temp[i]]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = 1; i < N+1; i++) {
-            if (!isUsed[i]) {
-                isUsed[i] = true;
-                ans[cur] = arr[i];
-                func(cur+1);
-                isUsed[i] = false;
-            }
-
+        for (int i = st; i < N; i++) {
+            temp[cur] = i;
+            func(cur+1,i+1);
         }
     }
 }
-
