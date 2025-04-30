76edeac830c734a98007e56a264c15755e7d26e2
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-30 10:22:05 +0900

    fix : update Boj15666 solution

--- javaBoj/boj/src/backTracking/Boj15666.java
+++ javaBoj/boj/src/backTracking/Boj15666.java
@@ -1,64 +1,62 @@
 package backTracking;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Boj15666 {
     static int N;
     static int M;
-    static int[] num;
     static int[] arr;
-    static Set<String> resultSet = new LinkedHashSet<>();
+    static int[] temp;
+    static boolean[] isused;
+
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException{
         Boj15666 process = new Boj15666();
         process.run();
-
     }
 
-    public void run() throws IOException {
+    private void run() throws IOException{
         init();
-        func(0, 0);
-        for (String res : resultSet) {
-            sb.append(res).append("\n");
-        }
+        func(0,0);
         System.out.println(sb.toString());
     }
 
-    public void init() throws IOException{
+    private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        num = new int[N];
-        arr = new int[M];
-        StringTokenizer ss = new StringTokenizer(bf.readLine());
+        arr = new int[N];
+        temp = new int[N];
+        isused = new boolean[N];
+        st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < N; i++) {
-            num[i] = Integer.parseInt(ss.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(num);
+
+        Arrays.sort(arr);
+
     }
 
-    public void func(int cur, int st) {
+    private void func(int cur, int st) {
         if (cur == M) {
-            StringBuilder temp = new StringBuilder();
             for (int i = 0; i < M; i++) {
-                temp.append(num[arr[i]]).append(" ");
+                sb.append(temp[i]).append(" ");
             }
-            String result = temp.toString().trim();
-            if (!resultSet.contains(result)) {
-                resultSet.add(result);
-            }
+            sb.append("\n");
             return;
         }
-
+        int tmp = 0;
         for (int i = st; i < N; i++) {
-            arr[cur] = i;
-            func(cur+1,i);
+            if(tmp != arr[i]) {
+                temp[cur] = arr[i];
+                tmp = arr[i];
+                func(cur + 1,i);
+            }
         }
     }
 }
