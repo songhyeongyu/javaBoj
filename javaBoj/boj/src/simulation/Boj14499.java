0caae744e1ddaf6cd0e6aa0bb2167317d4098b57
song hyeongyu <lit3207@ajou.ac.kr>
2025-05-19 13:19:13 +0900

    feat : add Boj14499 solution

--- javaBoj/boj/src/simulation/Boj14499.java
+++ javaBoj/boj/src/simulation/Boj14499.java
@@ -5,17 +5,26 @@ import java.util.*;
 
 public class Boj14499 {
 
-    //결국 1,1에 오는 놈이 무엇인지
-    static int[][] dice = {{-1, 0, -1}, {0, 0, 0}, {-1, 0, -1}, {-1, 0, -1}};
-    static int[][] board;
     static int N;
     static int M;
     static int x;
     static int y;
-    static int K;
+    static int k;
+
+    static int[][] board;
+
     static int[] cmd;
 
-    //1. 남쪽으로 굴렸을 떄
+    static int[][] dice = {
+        {0},
+        {0, 0, 0},
+        {0},
+        {0}
+    };
+
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+
     public static void main(String[] args) throws IOException{
         Boj14499 process = new Boj14499();
         process.run();
@@ -26,18 +35,17 @@ public class Boj14499 {
         makeDice();
     }
 
-    private void init() throws IOException {
+    private void init() throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         x = Integer.parseInt(st.nextToken());
         y = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
         board = new int[N][M];
-        cmd = new int[K];
+        cmd = new int[k];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(bf.readLine());
             for (int j = 0; j < M; j++) {
@@ -45,133 +53,78 @@ public class Boj14499 {
             }
         }
         st = new StringTokenizer(bf.readLine());
-
-        for (int i = 0; i < K; i++) {
+        for (int i = 0; i < k; i++) {
             cmd[i] = Integer.parseInt(st.nextToken());
         }
     }
 
+
     private void makeDice() {
+        dice[1][1] = board[x][y];
 
-        for (int i = 0; i < K; i++) {
-            int num = cmd[i];
+        for (int c : cmd) {
+            int nx = x + dx[c - 1];
+            int ny = y + dy[c - 1];
 
-            if (num == 1) {
-                rollEast();
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                continue;
             }
-            else if (num == 2) {
+
+            if (c == 1) {
+                rollEast();
+            } else if (c == 2) {
                 rollWest();
-            } else if (num == 3) {
+            } else if (c == 3) {
                 rollNorth();
-            } else if (num == 4) {
+
+            }else {
                 rollSouth();
             }
-
-
-
-//            System.out.println(Arrays.deepToString(dice));
-
+            x = nx;
+            y = ny;
+            makeBoard();
+            System.out.println(dice[3][0]);
         }
-
     }
 
-    private void rollNorth() {
-        if (!validRange(x - 1, y)) return;
-        x--;
-        //        static int[][] dice = {{-1, 0, -1}, {0, 0, 0}, {-1, 0, -1}, {-1, 0, -1}};
-
-        int temp = dice[0][1];
-        int temp2 = dice[1][1];
-        int temp3 = dice[2][1];
-        int temp4 = dice[3][1];
-
-        dice[0][1] = temp4;
-        dice[1][1] = temp;
-        dice[2][1] = temp2;
-        dice[3][1] = temp3;
-
-        process();
-        System.out.println(dice[3][1]);
-    }
-
-    private void rollSouth() {
-        if (!validRange(x + 1, y)) return;
-        x++;
-
-
-        int temp = dice[0][1];
-        int temp2 = dice[1][1];
-        int temp3 = dice[2][1];
-        int temp4 = dice[3][1];
-
-        dice[0][1] = temp2;
-        dice[1][1] = temp3;
-        dice[2][1] = temp4;
-        dice[3][1] = temp;
-        process();
-        System.out.println(dice[3][1]);
-
-    }
-
-
-    private void rollEast() {
-        if (!validRange(x, y + 1)) return;
-        y++;
-//        static int[][] dice = {{-1, 0, -1}, {0, 0, 0}, {-1, 0, -1}, {-1, 0, -1}};
-
-        int temp = dice[1][0];
-        int temp2 = dice[1][1];
-        int temp3 = dice[1][2];
-        int temp4 = dice[3][1];
-
-
-        dice[1][1] = temp3;
-        dice[1][0] = temp2;
-        dice[1][2] = temp4;
-        dice[3][1] = temp;
-
-        process();
-        System.out.println(dice[3][1]);
-
-
-    }
-
-    private void rollWest() {
-        if (!validRange(x, y - 1)) return;
-        y--;
-        //        static int[][] dice = {{-1, 0, -1}, {0, 0, 0}, {-1, 0, -1}, {-1, 0, -1}};
-
-        int temp = dice[1][0];
-        int temp2 = dice[1][1];
-        int temp3 = dice[1][2];
-        int temp4 = dice[3][1];
-
-
-        dice[1][0] = temp4;
-        dice[1][1] = temp;
-        dice[1][2] = temp2;
-        dice[3][1] = temp3;
-
-        process();
-        System.out.println(dice[3][1]);
-    }
-
-    private boolean validRange(int nx, int ny) {
-        return nx >= 0 && nx < N && ny >= 0 && ny < M;
-    }
-
-    private void process() {
-        if(board[x][y] != 0) {
+    private static void makeBoard() {
+        if (board[x][y] == 0) {
+            board[x][y] = dice[1][1];
+        }else{
             dice[1][1] = board[x][y];
             board[x][y] = 0;
         }
-        else {
-            board[x][y] = dice[1][1];
-        }
     }
 
+    private void rollEast() {
+        int temp = dice[1][1];
+        dice[1][1] = dice[1][2];
+        dice[1][2] = dice[3][0];
+        dice[3][0] = dice[1][0];
+        dice[1][0] = temp;
+    }
 
+    private void rollWest() {
+        int temp = dice[3][0];
+        dice[3][0] = dice[1][2];
+        dice[1][2] = dice[1][1];
+        dice[1][1] = dice[1][0];
+        dice[1][0] = temp;
+    }
+
+    private void rollNorth() {
+        int temp = dice[1][1];
+        dice[1][1] = dice[0][0];
+        dice[0][0] = dice[3][0];
+        dice[3][0] = dice[2][0];
+        dice[2][0] = temp;
+    }
+
+    private void rollSouth() {
+        int temp = dice[0][0];
+        dice[0][0] = dice[1][1];
+        dice[1][1] = dice[2][0];
+        dice[2][0] = dice[3][0];
+        dice[3][0] = temp;
+    }
 }
-
-
-
