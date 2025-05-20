449204f7214c6272f5aa28d247927b5c9b850b36
song hyeongyu <lit3207@ajou.ac.kr>
2025-05-20 13:41:18 +0900

    feat : revise to Boj1715

--- javaBoj/boj/src/priorityque/Boj1715.java
+++ javaBoj/boj/src/priorityque/Boj1715.java
@@ -1,72 +1,42 @@
 package priorityque;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 public class Boj1715 {
-    static PriorityQueue<Integer> pq;
 
     static int N;
+    static int num;
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException{
         Boj1715 process = new Boj1715();
         process.run();
-
     }
-
-    /**
-     * 5
-     * 73
-     * 18
-     * 14
-     * 24
-     * 12
-     */
 
     private void run() throws IOException {
         init();
-        calculateCard();
+        makeCard();
+        System.out.println(num);
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(bf.readLine());
-        pq = new PriorityQueue<>();
-        for (int i = 0; i < N; i++) {
-            pq.add(Integer.parseInt(bf.readLine()));
-        }
 
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(bf.readLine());
+            pq.offer(num);
+        }
     }
 
-    private void calculateCard() {
-        if (pq.size() == 1) {
-            System.out.println(pq.poll());
-
-            return;
+    private void makeCard() {
+        while (pq.size() != 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+            num += first + second;
+            pq.offer(first+second);
         }
-
-        int ans = 0;
-        int temp = 0;
-        while (true) {
-            if (pq.size() == 1) {
-                break;
-            }
-            if (pq.size() >= 2) {
-                int first = pq.poll();
-                int second = pq.poll();
-                temp = first + second;
-                ans += first + second;
-                pq.add(temp);
-            }
-
-        }
-
-        System.out.println(ans);
-
 
     }
 }
