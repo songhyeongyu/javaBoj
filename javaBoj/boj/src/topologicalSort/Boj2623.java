b471a1fdb7adde0c69cd3c9a1b8beb1202d523a4
song hyeongyu <lit3207@ajou.ac.kr>
2025-05-22 13:18:53 +0900

    feat : add Boj2623 solution

--- javaBoj/boj/src/topologicalSort/Boj2623.java
+++ javaBoj/boj/src/topologicalSort/Boj2623.java
@@ -3,93 +3,82 @@ package topologicalSort;
 import java.io.*;
 import java.util.*;
 
-public class Boj2623 {
+class Boj2623 {
+
     static int N;
     static int M;
-    static int[] indegree;
-    static List<Integer> result;
-    static List<List<Integer>> graph;
 
-    public static void main(String[] args) throws IOException {
+    static int[] indegree;
+
+    static List<List<Integer>> graph = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException{
         Boj2623 process = new Boj2623();
         process.run();
     }
 
     private void run() throws IOException {
         init();
-        topoSort();
-        printResult();
+        sort();
     }
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-
         indegree = new int[N + 1];
-
-        graph = new ArrayList<>();
-        result = new ArrayList<>();
-
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(bf.readLine());
+            int n = Integer.parseInt(st.nextToken());
 
-            int k = Integer.parseInt(st.nextToken());
-
-            int first = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < k - 1; j++) {
-                int second = Integer.parseInt(st.nextToken());
-                graph.get(first).add(second);
-                indegree[second]++;
-                first = second;
+            int parent = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < n - 1; j++) {
+                int child = Integer.parseInt(st.nextToken());
+                graph.get(parent).add(child);
+                indegree[child]++;
+                parent = child;
             }
         }
 
     }
 
-    private void topoSort() {
-        Queue<Integer> queue = new LinkedList<>();
+    private void sort() {
+        Queue<Integer> que = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        List<Integer> result = new ArrayList<>();
 
         for (int i = 1; i <= N; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
-            }
+            if(indegree[i] == 0)
+                que.offer(i);
         }
 
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-
+        while (!que.isEmpty()) {
+            int cur = que.poll();
             for (int nxt : graph.get(cur)) {
-                if (indegree[nxt] > 0) {
+                if (indegree[nxt] >= 0) {
                     indegree[nxt]--;
                     if (indegree[nxt] == 0) {
-                        queue.offer(nxt);
+                        que.offer(nxt);
                     }
                 }
-
             }
+            sb.append(cur).append("\n");
             result.add(cur);
         }
-
-
-
-    }
-
-    private void printResult() {
         if (result.size() != N) {
             System.out.println(0);
             return;
         }
+        System.out.println(sb.toString());
 
-        for (Integer integer : result) {
-            System.out.println(integer);
-        }
     }
 }
+
+
