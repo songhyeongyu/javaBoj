a717f590eb42f4b3a8e95790cffde13781c4ce74
song hyeongyu <lit3207@ajou.ac.kr>
2025-05-22 11:27:45 +0900

    feat : add Boj1717 solution

--- javaBoj/boj/src/unionfind/Boj1717.java
+++ javaBoj/boj/src/unionfind/Boj1717.java
@@ -1,20 +1,19 @@
 package unionfind;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Boj1717 {
+class Boj1717 {
+
     static int N;
     static int M;
+
     static int[] p;
 
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         Boj1717 process = new Boj1717();
         process.run();
+
     }
 
     private void run() throws IOException {
@@ -23,24 +22,31 @@ public class Boj1717 {
 
     private void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         p = new int[N + 1];
-
-        Arrays.fill(p, -1);
-
+        Arrays.fill(p,-1);
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(bf.readLine());
 
             int cmd = Integer.parseInt(st.nextToken());
-            int parent = Integer.parseInt(st.nextToken());
-            int child = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
+            if (cmd == 1) {
+                if (find(u) == find(v)) {
+                    System.out.println("YES");
+                }else{
+                    System.out.println("NO");
+                }
+            }
 
+            else{
+                union(u, v);
+            }
         }
     }
 
@@ -49,20 +55,23 @@ public class Boj1717 {
         if (p[x] < 0) {
             return x;
         }
-        return p[x] = find(p[x]);
+        return find(p[x]);
     }
 
     private boolean union(int u, int v) {
         u = find(u);
         v = find(v);
+
         if (u == v) {
             return false;
         }
+
         if (p[v] < p[u]) {
             int temp = u;
             u = v;
             v = temp;
         }
+
         if (p[u] == p[v]) {
             p[u]--;
         }
@@ -70,4 +79,7 @@ public class Boj1717 {
         return true;
     }
 
+
 }
+
+
